3eedea8ebfef5454cffe56d30d8189a8efcacbe6
LYChubby <f4uzi345@gmail.com>
2023-06-15 10:47:42 +0700

    Membuat Tampilan Output

--- StackUsingArray/StackUsingArray.cpp
+++ StackUsingArray/StackUsingArray.cpp
@@ -54,6 +54,17 @@ public:
 int main() {
 	program s;
 
-
+	while (true) {
+		cout << endl;
+		cout << "\n***Stack Menu***\n";
+		cout << "1. Push\n";
+		cout << "2. Pop\n";
+		cout << "3. Display\n";
+		cout << "4. Exit\n";
+		cout << "\nEnter Your Choice : ";
+		string input;
+		getline(cin, input);
+		char ch = (input.empty() ? '0' : input[0]);
+	}
 }
 
